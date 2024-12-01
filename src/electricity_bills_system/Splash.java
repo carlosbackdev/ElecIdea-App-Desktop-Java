6f0992a1b6f4d59a4c6cad21975fb7d9149061f6
carlos <carloslospinos@gmail.com>
2024-12-01 18:54:49 +0100

    Frame Inicio Carga

--- src/electricity_bills_system/Splash.java
+++ src/electricity_bills_system/Splash.java
@@ -7,14 +7,14 @@ public class Splash extends JFrame  implements Runnable{
     Thread t;
     Splash(){
     
-        ImageIcon img1= new ImageIcon(ClassLoader.getSystemResource("images/portada.jpg"));
+        ImageIcon img1= new ImageIcon(ClassLoader.getSystemResource("images/portada2.jpg"));
         Image i1=img1.getImage().getScaledInstance(1200,800, Image.SCALE_DEFAULT);
         ImageIcon imagen1= new ImageIcon(i1);
         JLabel image=new JLabel(imagen1);
         add(image);
         
         int x=1;
-        for(int i=2;i<700; i+=4,x+=1)
+        for(int i=2;i<820; i+=4,x+=1)
         setSize(i +x, i);
         setLocationRelativeTo(null);
         try{
