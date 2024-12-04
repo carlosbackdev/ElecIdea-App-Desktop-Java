e7ee7868dba31325a688126be81e29d0b3e9e95d
carlos <carloslospinos@gmail.com>
2024-12-04 23:15:02 +0100

    CONECTADA A BASE DE DATOS REMOTA

--- src/electricity_bills_system/Connect.java
+++ src/electricity_bills_system/Connect.java
@@ -8,7 +8,7 @@ public class Connect {
     Statement s;
     Connect(){
         try{
-            c = DriverManager.getConnection("jdbc:mysql:///ebs","root","12345");
+            c = DriverManager.getConnection("jdbc:mysql://sql7.freemysqlhosting.net:3306/sql7749848","sql7749848","335YxJ5li8");
             s=c.createStatement();
         }catch(Exception e){
             e.printStackTrace();
