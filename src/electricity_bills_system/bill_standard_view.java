1419d7b0faa7c380d6a362d4778271c58f7f2186
carloslospinos <carloslospinos@A3-DIT-013.digitechmad.local>
2024-12-16 20:03:30 +0100

    clase branch

--- src/electricity_bills_system/bill_standard_view.java
+++ src/electricity_bills_system/bill_standard_view.java
@@ -4,4 +4,11 @@ package electricity_bills_system;
 
 public class bill_standard_view {
     
+    bill_standard_view(String ID_2,String NAME,String ADDRESS,String HOUR,String DATE,String NUMBER_MATERIAL,String TOTAL_MATERIAL,String PARAMETROS,String NUMBER_FACTURA,String TOTAL_BILL){
+    
+    }
+    
+    public static void main(String[]args){
+        new bill_standard_view("","","","","","","","","","");
+    }
 }
